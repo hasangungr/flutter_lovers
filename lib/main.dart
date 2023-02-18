e9e26be26d7261f63d8b1cbb876f78bd67181f15
hasangungr <hasangungr1@gmail.com>
2023-02-18 22:18:48 +0300

    Import Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'view/sign_in_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+// ...
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,11 +19,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Fluter Lovers",
-      debugShowCheckedModeBanner: false,
-      
-      home: SignInView()
-    );
+        title: "Fluter Lovers",
+        debugShowCheckedModeBanner: false,
+        home: SignInView());
   }
 }
-
